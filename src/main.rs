14081b3f1ef5d603b5bd358f6f99c65398e23c9e
han-tyumi <mmchamp95@gmail.com>
2020-12-28 16:32:04 -0500

    finish basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
-use std::collections::HashMap;
+use std::{
+    collections::{BinaryHeap, HashMap, HashSet},
+    iter::FromIterator,
+};
 
 use dialoguer::{Input, MultiSelect};
 
+use rand::{self, Rng};
+
+pub trait RemoveRandom {
+    type Item;
+
+    fn remove_random<R: Rng>(&mut self, rng: &mut R) -> Option<Self::Item>;
+}
+
+impl<T> RemoveRandom for Vec<T> {
+    type Item = T;
+
+    fn remove_random<R: Rng>(&mut self, rng: &mut R) -> Option<Self::Item> {
+        if self.is_empty() {
+            None
+        } else {
+            let index = rng.gen_range(0..self.len());
+            Some(self.swap_remove(index))
+        }
+    }
+}
+
 fn main() {
+    let mut rng = rand::thread_rng();
+
     let names = Input::new()
         .with_prompt("Enter names separated by commas")
         .validate_with(|input: &String| {
@@ -15,7 +41,7 @@ fn main() {
         .interact_text()
         .unwrap();
 
-    let names: Vec<_> = names
+    let names: HashSet<_> = names
         .split(",")
         .filter_map(|name| {
             let name = name.trim();
@@ -27,11 +53,13 @@ fn main() {
         })
         .collect();
 
-    let mut exclusions = HashMap::new();
     let mut pre_check = HashMap::new();
+    let mut selections = HashMap::new();
+    let mut order = HashMap::new();
+    let mut next_pick: (usize, HashSet<&str>) = (usize::MAX, HashSet::new());
 
     for name in &names {
-        let pre_checked = pre_check.entry(*name).or_insert(vec![]);
+        let pre_checked = pre_check.entry(*name).or_insert(HashSet::new());
 
         let items: Vec<_> = names
             .iter()
@@ -52,13 +80,86 @@ fn main() {
             .items_checked(&items)
             .interact()
             .unwrap();
-        let selected: Vec<_> = selected.iter().map(|i| items[*i].0).collect();
+        let selected: HashSet<_> = selected.iter().map(|i| items[*i].0).collect();
 
         for selected_name in &selected {
-            let pre_select_names = pre_check.entry(*selected_name).or_insert(vec![]);
-            pre_select_names.push(*name);
+            let pre_select_names = pre_check.entry(*selected_name).or_insert(HashSet::new());
+            pre_select_names.insert(*name);
         }
 
-        exclusions.insert(*name, selected.clone());
+        let mut order_key = BinaryHeap::new();
+        let selections_for_name: HashSet<_> = items
+            .iter()
+            .copied()
+            .filter_map(|item| {
+                let (name, ..) = item;
+
+                if selected.contains(name) {
+                    None
+                } else {
+                    order_key.push(name);
+                    Some(name)
+                }
+            })
+            .collect();
+
+        let order_key = order_key.into_sorted_vec().join("");
+        let num_names = selections_for_name.len();
+
+        let orders = order.entry(order_key).or_insert(HashSet::new());
+        orders.insert(*name);
+
+        if (num_names < next_pick.0)
+            || (num_names == next_pick.0 && orders.len() >= next_pick.1.len())
+        {
+            next_pick = (num_names, orders.clone());
+        }
+
+        selections.insert(*name, selections_for_name);
     }
+
+    let mut result = HashMap::new();
+
+    loop {
+        let (.., names) = next_pick.clone();
+        next_pick = (usize::MAX, HashSet::new());
+        order.clear();
+
+        for name in names {
+            let picks = selections.remove(name).unwrap();
+            let picks = Vec::from_iter(picks);
+
+            let index = rng.gen_range(0..picks.len());
+            let pick = picks[index];
+
+            result.insert(name, pick);
+
+            if selections.is_empty() {
+                break;
+            }
+
+            for (name, picks) in &mut selections {
+                picks.remove(pick);
+
+                let order_key: BinaryHeap<_> = picks.iter().copied().collect();
+                let num_names = order_key.len();
+                let order_key = order_key.into_sorted_vec().join("");
+
+                let orders = order.entry(order_key).or_insert(HashSet::new());
+                orders.insert(*name);
+
+                if (num_names < next_pick.0)
+                    || (num_names == next_pick.0 && orders.len() >= next_pick.1.len())
+                {
+                    next_pick = (num_names, orders.clone());
+                }
+            }
+        }
+
+        if selections.is_empty() {
+            break;
+        }
+    }
+
+    println!("{:#?}", result);
 }
