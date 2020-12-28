6a8ddcc915ad09b45aa40b10ac8441d05419d869
han-tyumi <mmchamp95@gmail.com>
2020-12-28 16:44:10 -0500

    add regenerate prompt

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{
     iter::FromIterator,
 };
 
-use dialoguer::{Input, MultiSelect};
+use dialoguer::{Confirm, Input, MultiSelect};
 
 use rand::{self, Rng};
 
@@ -57,6 +57,8 @@ fn main() {
     let mut selections = HashMap::new();
     let mut order = HashMap::new();
     let mut next_pick: (usize, HashSet<&str>) = (usize::MAX, HashSet::new());
+
+    println!();
 
     for name in &names {
         let pre_checked = pre_check.entry(*name).or_insert(HashSet::new());
@@ -118,48 +120,62 @@ fn main() {
         selections.insert(*name, selections_for_name);
     }
 
+    println!();
+
     let mut result = HashMap::new();
 
     loop {
-        let (.., names) = next_pick.clone();
-        next_pick = (usize::MAX, HashSet::new());
-        order.clear();
+        let mut curr_selections = selections.clone();
 
-        for name in names {
-            let picks = selections.remove(name).unwrap();
-            let picks = Vec::from_iter(picks);
+        loop {
+            let (.., names) = next_pick.clone();
+            next_pick = (usize::MAX, HashSet::new());
+            order.clear();
 
-            let index = rng.gen_range(0..picks.len());
-            let pick = picks[index];
+            for name in names {
+                let picks = curr_selections.remove(name).unwrap();
+                let picks = Vec::from_iter(picks);
 
-            result.insert(name, pick);
+                let index = rng.gen_range(0..picks.len());
+                let pick = picks[index];
 
-            if selections.is_empty() {
-                break;
+                result.insert(name, pick);
+
+                if curr_selections.is_empty() {
+                    break;
+                }
+
+                for (name, picks) in &mut curr_selections {
+                    picks.remove(pick);
+
+                    let order_key: BinaryHeap<_> = picks.iter().copied().collect();
+                    let num_names = order_key.len();
+                    let order_key = order_key.into_sorted_vec().join("");
+
+                    let orders = order.entry(order_key).or_insert(HashSet::new());
+                    orders.insert(*name);
+
+                    if (num_names < next_pick.0)
+                        || (num_names == next_pick.0 && orders.len() >= next_pick.1.len())
+                    {
+                        next_pick = (num_names, orders.clone());
+                    }
+                }
             }
 
-            for (name, picks) in &mut selections {
-                picks.remove(pick);
-
-                let order_key: BinaryHeap<_> = picks.iter().copied().collect();
-                let num_names = order_key.len();
-                let order_key = order_key.into_sorted_vec().join("");
-
-                let orders = order.entry(order_key).or_insert(HashSet::new());
-                orders.insert(*name);
-
-                if (num_names < next_pick.0)
-                    || (num_names == next_pick.0 && orders.len() >= next_pick.1.len())
-                {
-                    next_pick = (num_names, orders.clone());
-                }
+            if curr_selections.is_empty() {
+                break;
             }
         }
 
-        if selections.is_empty() {
+        println!("Result:\n{:#?}", result);
+
+        if !Confirm::new()
+            .with_prompt("Regenerate?")
+            .interact()
+            .unwrap()
+        {
             break;
         }
     }
-
-    println!("{:#?}", result);
 }
